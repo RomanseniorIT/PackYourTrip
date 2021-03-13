f5c3d4718243dc4972295709de906bc582ca05d5
Roman Lazuko <romanlazuko@gmail.com>
2021-03-13 23:58:58 +0300

    Fix TripVM

--- app/src/main/java/com/example/packyourtrip/di/module/ViewModelModule.kt
+++ app/src/main/java/com/example/packyourtrip/di/module/ViewModelModule.kt
@@ -3,11 +3,14 @@ package com.example.packyourtrip.di.module
 import androidx.lifecycle.ViewModel
 import com.example.packyourtrip.di.annotation.ViewModelKey
 import com.example.packyourtrip.ui.auth.AuthViewModel
+import com.example.packyourtrip.ui.checklist.things.TripCheckListViewModel
+import com.example.packyourtrip.ui.main.defaultlists.SavedListsViewModel
 import com.example.packyourtrip.ui.main.trip.TripViewModel
 import com.example.packyourtrip.ui.splash.SplashViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import javax.inject.Singleton
 
 @Module
 abstract class ViewModelModule {
@@ -25,6 +28,7 @@ abstract class ViewModelModule {
     @Binds
     @IntoMap
     @ViewModelKey(TripCheckListViewModel::class)
+    @Singleton
     abstract fun tripCheckListViewModel(viewModel: TripCheckListViewModel): ViewModel
 
     @Binds
