4ae0dddd436e37b5761e16c9aadd0dec7f81f096
Alexander Podkopaev <p-alex1990@mail.ru>
2021-03-13 12:03:00 +0500

    Create model to saved lists

---
+++ app/src/main/java/com/example/packyourtrip/data/model/SavedToDosModel.kt
@@ -0,0 +1,11 @@
+package com.example.packyourtrip.data.model
+
+import com.google.firebase.firestore.DocumentId
+
+data class SavedToDosModel(
+    @DocumentId
+    val id: String? = null,
+    val title: String? = null,
+    val toDos: List<String> = mutableListOf(),
+    val owner: List<String> = mutableListOf()
+)
