e3bb23249996ced387a39d741ecd2c40319fb72a
tosha <tosha170@outlook.com>
2021-03-13 19:45:30 +0300

    add dialog fragment
    диалог пока что не работает

--- app/src/main/java/com/example/packyourtrip/ui/dialog/CreateTripDialog.kt
+++ app/src/main/java/com/example/packyourtrip/ui/dialog/CreateTripDialog.kt
@@ -1,7 +1,28 @@
 package com.example.packyourtrip.ui.dialog
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
+import com.example.packyourtrip.R
 
 class CreateTripDialog: DialogFragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog?.setTitle("Создать поездку")
+        val v: View = inflater.inflate(R.layout.fragment_dialog, container)
+        val btnSave: Button = v.findViewById(R.id.btn_save)
+        btnSave.setOnClickListener { onClickBtnSave() }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun onClickBtnSave(){
+
+    }
 }
