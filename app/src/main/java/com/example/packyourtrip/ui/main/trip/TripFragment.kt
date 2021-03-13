f4a51912208d9a4b42deaec25802d6453e695cd1
tosha <tosha170@outlook.com>
2021-03-13 22:08:00 +0300

    fix dialog
    диалог работает, но не сохраняет данные в бд

--- app/src/main/java/com/example/packyourtrip/ui/main/trip/TripFragment.kt
+++ app/src/main/java/com/example/packyourtrip/ui/main/trip/TripFragment.kt
@@ -3,10 +3,13 @@ package com.example.packyourtrip.ui.main.trip
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.packyourtrip.R
+import com.example.packyourtrip.ui.dialog.CreateTripDialog
 import com.example.packyourtrip.ui.main.TripListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class TripFragment : Fragment(R.layout.fragment_trip), TripListener {
@@ -14,6 +17,8 @@ class TripFragment : Fragment(R.layout.fragment_trip), TripListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecycler(view)
+        val fabBtn: FloatingActionButton = view.findViewById(R.id.btn_add_trip)
+        fabBtn.setOnClickListener { onClickFabBtn() }
     }
 
     private fun initRecycler(view: View) {
@@ -22,6 +27,10 @@ class TripFragment : Fragment(R.layout.fragment_trip), TripListener {
         recyclerTrips.adapter = TripAdapter(this)
 
 //        findNavController().navigate()
+    }
+
+    private fun onClickFabBtn() {
+        CreateTripDialog().show(parentFragmentManager, "CreateTripDialog")
     }
 
     companion object {
